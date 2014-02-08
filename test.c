293c6cb06dc167c6fc32daf5292ad17e89443102
midexiamu <midexiamucolor@gamil.com>
2014-02-08 11:41:18 +0900

    Create test.c

---
+++ test.c
@@ -0,0 +1,18 @@
+
+#include  <stdio.h>
+
+int main()
+{
+	char *s = "abc efd";
+	int i;
+	for (i = 0; s[i] != '\0'; i++){
+		if (s[i] == ' '){
+			putchar('\n');
+		}else{
+			putchar(s[i]);
+		}
+	}
+	putchar('\n');
+	
+	return 0;
+}
